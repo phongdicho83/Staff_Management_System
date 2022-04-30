fc5f8ca9d921127ea8efa4d7a02baf8955b55112
ChiThien249 <104313171+ChiThien249@users.noreply.github.com>
2022-04-30 23:04:34 +0700

    Create NhanVienBUS.java

---
+++ src/staff_management_system/BUS/NhanVienBUS.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package staff_management_system.BUS;
+
+import Helpers.MessageDialogHelper;
+import java.util.ArrayList;
+import staff_management_system.DAO.NhanVienDAO;
+import staff_management_system.DTO.NhanVien;
+import staff_management_system.UI.NhanVienUI;
+
+/**
+ *
+ * @author Utech
+ */
+public class NhanVienBUS {
+    public ArrayList<NhanVien> getNhanViens() throws Exception {
+        NhanVienDAO dao = new NhanVienDAO();
+        return NhanVienUI.listNhanVien = dao.getNhanViens();
+    }
+
+    public boolean addNhanVien(NhanVien nhanVien) {
+        NhanVienDAO dao = new NhanVienDAO();
+        if (dao.addNhanVien(nhanVien)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean delNhanVien(String MaNV) {
+        NhanVienDAO dao = new NhanVienDAO();
+        if (!dao.delNhanVien(MaNV)) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean updateNhanVien(NhanVien nhanVien) {
+        NhanVienDAO dao = new NhanVienDAO();
+        if (!dao.updateNhanVien(nhanVien)) {
+            return false;
+        }
+        return true;
+    }
+}
