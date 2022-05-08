d90b2b61620f7e7f3ce1c191cc2433ffa5a83a31
tfong83 <phongdicho83@gmail.com>
2022-05-09 00:31:56 +0700

    Update staff_salary_management.sql

--- docs/staff_salary_management.sql
+++ docs/staff_salary_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2022 at 04:10 PM
+-- Generation Time: May 08, 2022 at 07:31 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
@@ -28,34 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `bangchamcong` (
-  `MaChamCong` varchar(10) NOT NULL,
-  `MaNV` varchar(10) NOT NULL,
-  `NgayThangNam` date NOT NULL,
-  `TrangThai` varchar(10) NOT NULL
+  `machamcong` varchar(10) NOT NULL,
+  `manv` varchar(10) NOT NULL,
+  `ngaythangnam` date NOT NULL,
+  `trangthai` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `bangchamcong`
---
-
-INSERT INTO `bangchamcong` (`MaChamCong`, `MaNV`, `NgayThangNam`, `TrangThai`) VALUES
-('CC01', 'NV01', '2022-02-19', 'Nghỉ phép'),
-('CC02', 'NV03', '2022-04-09', 'Vắng'),
-('CC03', 'NV02', '2022-10-08', 'Nghỉ phép'),
-('CC04', 'NV05', '2022-04-28', 'Nghỉ phép'),
-('CC05', 'NV02', '2022-07-10', 'Nghỉ phép'),
-('CC06', 'NV03', '2022-09-13', 'Vắng'),
-('CC07', 'NV04', '2022-05-18', 'Nghỉ phép'),
-('CC08', 'NV05', '2022-01-24', 'Vắng'),
-('CC09', 'NV04', '2022-03-12', 'Nghỉ phép'),
-('CC10', 'NV04', '2022-06-22', 'Nghỉ phép'),
-('CC11', 'NV02', '2022-05-08', 'Nghỉ phép'),
-('CC12', 'NV01', '2022-06-29', 'Nghỉ phép'),
-('CC13', 'NV08', '2022-06-15', 'Vắng'),
-('CC14', 'NV08', '2022-12-12', 'Nghỉ phép'),
-('CC15', 'NV07', '2022-09-18', 'Vắng'),
-('CC16', 'NV06', '2022-04-11', 'Nghỉ phép'),
-('CC17', 'NV07', '2022-06-10', 'Vắng');
 
 -- --------------------------------------------------------
 
@@ -65,49 +42,26 @@ INSERT INTO `bangchamcong` (`MaChamCong`, `MaNV`, `NgayThangNam`, `TrangThai`) V
 
 CREATE TABLE `bangluongthang` (
   `MaNV` varchar(10) NOT NULL,
-  `MaHopDong` varchar(10) NOT NULL,
   `MaPhuCap` varchar(10) DEFAULT NULL,
   `MaTienThuong` varchar(10) DEFAULT NULL,
   `SoGioTangCa` varchar(10) NOT NULL,
   `LuongBiTru` varchar(10) NOT NULL,
-  `NgayThangNam` date NOT NULL
+  `ThangNam` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `bangluongthang`
 --
 
-INSERT INTO `bangluongthang` (`MaNV`, `MaHopDong`, `MaPhuCap`, `MaTienThuong`, `SoGioTangCa`, `LuongBiTru`, `NgayThangNam`) VALUES
-('NV01', 'HD01', 'PC02', 'TT04', '70', '0', '2019-08-16'),
-('NV02', 'HD08', 'PC01', 'TT04', '59', '2%', '2019-08-16'),
-('NV03', 'HD06', 'PC01', 'TT01', '0', '20%', '2019-08-16'),
-('NV04', 'HD07', 'PC02', 'TT02', '0', '2%', '2019-08-16'),
-('NV05', 'HD03', 'PC02', 'TT02', '55', '10%', '2019-08-16'),
-('NV06', 'HD02', 'PC01', 'TT01', '25', '0', '2019-08-16'),
-('NV07', 'HD05', 'PC02', 'TT01', '0', '20%', '2022-02-01'),
-('NV08', 'HD04', NULL, NULL, '70', '10%', '2019-08-16');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `chucvu`
---
-
-CREATE TABLE `chucvu` (
-  `MaChucVu` varchar(10) NOT NULL,
-  `TenChucVu` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `chucvu`
---
-
-INSERT INTO `chucvu` (`MaChucVu`, `TenChucVu`) VALUES
-('CV01', 'Giám đốc'),
-('CV02', 'Trưởng phòng'),
-('CV03', 'Quản lý dự án'),
-('CV04', 'Nhân viên cao cấp'),
-('CV05', 'Nhân viên');
+INSERT INTO `bangluongthang` (`MaNV`, `MaPhuCap`, `MaTienThuong`, `SoGioTangCa`, `LuongBiTru`, `ThangNam`) VALUES
+('NV01', 'PC02', 'TT04', '70', '0', '2019-08-16'),
+('NV02', 'PC01', 'TT04', '59', '2%', '2019-08-16'),
+('NV03', 'PC01', 'TT01', '0', '20%', '2019-08-16'),
+('NV04', 'PC02', 'TT02', '0', '2%', '2019-08-16'),
+('NV05', 'PC02', 'TT02', '55', '10%', '2019-08-16'),
+('NV06', 'PC01', 'TT01', '25', '0', '2019-08-16'),
+('NV07', 'PC02', 'TT01', '0', '20%', '2022-02-01'),
+('NV08', NULL, NULL, '70', '10%', '2019-08-16');
 
 -- --------------------------------------------------------
 
@@ -134,34 +88,6 @@ INSERT INTO `congviec` (`MaCongViec`, `TenCongViec`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `luonghopdong`
---
-
-CREATE TABLE `luonghopdong` (
-  `MaHopDong` varchar(10) NOT NULL,
-  `MaNV` varchar(10) NOT NULL,
-  `BacLuong` int(11) NOT NULL,
-  `NgayKyHopDong` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `luonghopdong`
---
-
-INSERT INTO `luonghopdong` (`MaHopDong`, `MaNV`, `BacLuong`, `NgayKyHopDong`) VALUES
-('HD01', 'NV01', 2, '2016-04-03'),
-('HD02', 'NV06', 3, '2013-02-09'),
-('HD03', 'NV05', 1, '2016-08-06'),
-('HD04', 'NV08', 1, '2017-07-10'),
-('HD05', 'NV07', 2, '2016-10-21'),
-('HD06', 'NV03', 1, '2022-12-12'),
-('HD07', 'NV04', 2, '2016-08-05'),
-('HD08', 'NV02', 3, '2013-04-19'),
-('HD09', 'NV09', 2, '2016-08-06');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `nguoithan`
 --
 
@@ -169,9 +95,18 @@ CREATE TABLE `nguoithan` (
   `MaNV` varchar(10) NOT NULL,
   `STT` int(11) NOT NULL,
   `TenNguoiThan` varchar(50) NOT NULL,
+  `MoiQuanHe` varchar(10) NOT NULL,
   `GioiTinh` varchar(5) NOT NULL,
   `NamSinh` varchar(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `nguoithan`
+--
+
+INSERT INTO `nguoithan` (`MaNV`, `STT`, `TenNguoiThan`, `MoiQuanHe`, `GioiTinh`, `NamSinh`) VALUES
+('NV01', 1, 'Kẹo Dẻo', 'Con', 'Nữ', '2002'),
+('NV01', 2, 'Kẹo Xoài', 'Con', 'Nam', '2001');
 
 -- --------------------------------------------------------
 
@@ -187,23 +122,23 @@ CREATE TABLE `nhanvien` (
   `SoDienThoai` varchar(15) NOT NULL,
   `GioiTinh` varchar(5) NOT NULL,
   `MaPhongBan` varchar(10) NOT NULL,
-  `MaChucVu` varchar(10) NOT NULL
+  `NgayVaoLam` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `nhanvien`
 --
 
-INSERT INTO `nhanvien` (`MaNV`, `Ho`, `Ten`, `NgaySinh`, `SoDienThoai`, `GioiTinh`, `MaPhongBan`, `MaChucVu`) VALUES
-('NV01', 'Vương Đại ', 'Nguyên', '2002-02-04', '0194121451', 'Nam', 'PB03', 'CV04'),
-('NV02', 'Bùi Thị Thanh', 'Bình', '2003-07-11', '0543141241', 'Nữ', 'PB01', 'CV02'),
-('NV03', 'Lê Thị Huỳnh', 'Như', '2003-08-11', '0214124325', 'Nữ', 'PB01', 'CV01'),
-('NV04', 'Hoàng Nam', 'Trung', '2002-03-29', '0414329841', 'Nam', 'PB03', 'CV05'),
-('NV05', 'Nguyễn Hoàng', 'Phúc', '2002-04-18', '0235325238', 'Nam', 'PB02', 'CV04'),
-('NV06', 'Nguyễn Thị Ngọc', 'Trâm', '2022-07-15', '0342598173', 'Nữ', 'PB03', 'CV05'),
-('NV07', 'Đặng Thị Thúy', 'Nga', '2022-01-04', '0119291477', 'Nữ', 'PB01', 'CV02'),
-('NV08', 'Phan Trung', 'Kiên', '2022-01-12', '0523852141', 'Nam', 'PB02', 'CV01'),
-('NV09', 'Phan Tiến', 'Nam', '2022-11-24', '0682465235', 'Nam', 'PB02', 'CV04');
+INSERT INTO `nhanvien` (`MaNV`, `Ho`, `Ten`, `NgaySinh`, `SoDienThoai`, `GioiTinh`, `MaPhongBan`, `NgayVaoLam`) VALUES
+('NV01', 'Vương Đại ', 'Nguyên', '2002-02-04', '0194121451', 'Nam', 'PB03', '2016-08-13'),
+('NV02', 'Bùi Thị Thanh', 'Bình', '2003-07-11', '0543141241', 'Nữ', 'PB01', '2018-10-12'),
+('NV03', 'Lê Thị Huỳnh', 'Như', '2003-08-11', '0214124325', 'Nữ', 'PB01', '2016-03-08'),
+('NV04', 'Hoàng Nam', 'Trung', '2002-03-29', '0414329841', 'Nam', 'PB03', '2018-01-10'),
+('NV05', 'Nguyễn Hoàng', 'Phúc', '2002-04-18', '0235325238', 'Nam', 'PB02', '2015-05-23'),
+('NV06', 'Nguyễn Thị Ngọc', 'Trâm', '2022-07-15', '0342598173', 'Nữ', 'PB03', '2017-07-15'),
+('NV07', 'Đặng Thị Thúy', 'Nga', '2022-01-04', '0119291477', 'Nữ', 'PB01', '2018-12-08'),
+('NV08', 'Phan Trung', 'Kiên', '2022-01-12', '0523852141', 'Nam', 'PB02', '2016-08-06'),
+('NV09', 'Phan Tiến', 'Nam', '2022-11-24', '0682465235', 'Nam', 'PB02', '2018-06-28');
 
 -- --------------------------------------------------------
 
@@ -278,8 +213,10 @@ INSERT INTO `tangca` (`MaNV`, `NgayThangNam`, `SoGioTangCa`) VALUES
 --
 
 CREATE TABLE `theodoitangluong` (
-  `MaHopDong` varchar(10) NOT NULL,
-  `LuongMoi` varchar(10) NOT NULL
+  `MaNV` varchar(10) NOT NULL,
+  `SoNgayLam` varchar(5) NOT NULL,
+  `Tang` varchar(5) NOT NULL,
+  `LuongTang` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -291,7 +228,7 @@ CREATE TABLE `theodoitangluong` (
 CREATE TABLE `tienthuong` (
   `MaTienThuong` varchar(10) NOT NULL,
   `TenThuong` varchar(20) NOT NULL,
-  `NgayThuong` date NOT NULL,
+  `Thang` varchar(2) NOT NULL,
   `SoTienThuong` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -299,11 +236,10 @@ CREATE TABLE `tienthuong` (
 -- Dumping data for table `tienthuong`
 --
 
-INSERT INTO `tienthuong` (`MaTienThuong`, `TenThuong`, `NgayThuong`, `SoTienThuong`) VALUES
-('TT01', 'Lễ Tết', '2022-02-08', '5000000'),
-('TT02', 'Quốc tế thiếu nhi', '2022-06-01', '3000000'),
-('TT03', 'Nhân viên của tháng', '2022-11-24', '7500000'),
-('TT04', 'Vượt KPI', '2022-04-02', '2000000');
+INSERT INTO `tienthuong` (`MaTienThuong`, `TenThuong`, `Thang`, `SoTienThuong`) VALUES
+('TT01', 'Lễ Tết', '2', '5000000'),
+('TT02', 'Quốc tế thiếu nhi', '6', '3000000'),
+('TT04', 'Vượt KPI', '20', '2000000');
 
 -- --------------------------------------------------------
 
@@ -315,23 +251,36 @@ CREATE TABLE `tongketchamcongthang` (
   `MaNV` varchar(10) NOT NULL,
   `SoKhongPhep` varchar(5) NOT NULL,
   `VangCoPhep` varchar(5) NOT NULL,
-  `LuongBiTru` varchar(10) NOT NULL
+  `LuongBiTru` varchar(10) NOT NULL,
+  `ThangNam` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tongketchamcongthang`
 --
 
-INSERT INTO `tongketchamcongthang` (`MaNV`, `SoKhongPhep`, `VangCoPhep`, `LuongBiTru`) VALUES
-('NV01', '0', '2', '0'),
-('NV02', '0', '3', '2%'),
-('NV03', '2', '0', '20%'),
-('NV04', '0', '3', '2%'),
-('NV05', '1', '1', '10%'),
-('NV06', '0', '1', '0'),
-('NV07', '2', '0', '20%'),
-('NV08', '1', '1', '10%'),
-('NV09', '0', '0', '0');
+INSERT INTO `tongketchamcongthang` (`MaNV`, `SoKhongPhep`, `VangCoPhep`, `LuongBiTru`, `ThangNam`) VALUES
+('NV01', '0', '2', '0', ''),
+('NV02', '0', '3', '2%', ''),
+('NV03', '2', '0', '20%', ''),
+('NV04', '0', '3', '2%', ''),
+('NV05', '1', '1', '10%', ''),
+('NV06', '0', '1', '0', ''),
+('NV07', '2', '0', '20%', ''),
+('NV08', '1', '1', '10%', ''),
+('NV09', '0', '0', '0', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tongkettangca`
+--
+
+CREATE TABLE `tongkettangca` (
+  `MaNV` varchar(10) NOT NULL,
+  `ThangNam` varchar(10) NOT NULL,
+  `SoGioTangCa` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -341,39 +290,22 @@ INSERT INTO `tongketchamcongthang` (`MaNV`, `SoKhongPhep`, `VangCoPhep`, `LuongB
 -- Indexes for table `bangchamcong`
 --
 ALTER TABLE `bangchamcong`
-  ADD PRIMARY KEY (`MaChamCong`),
-  ADD KEY `fkChamcongNV` (`MaNV`);
+  ADD PRIMARY KEY (`machamcong`),
+  ADD KEY `fkbangchamcong` (`manv`);
 
 --
 -- Indexes for table `bangluongthang`
 --
 ALTER TABLE `bangluongthang`
   ADD PRIMARY KEY (`MaNV`),
-  ADD UNIQUE KEY `MaHopDong` (`MaHopDong`),
-  ADD UNIQUE KEY `MaHopDong_2` (`MaHopDong`),
-  ADD UNIQUE KEY `MaHopDong_5` (`MaHopDong`),
-  ADD KEY `MaHopDong_3` (`MaHopDong`),
-  ADD KEY `MaHopDong_4` (`MaHopDong`),
   ADD KEY `fkPhucap` (`MaPhuCap`),
   ADD KEY `fkTienthuong` (`MaTienThuong`);
-
---
--- Indexes for table `chucvu`
---
-ALTER TABLE `chucvu`
-  ADD PRIMARY KEY (`MaChucVu`);
 
 --
 -- Indexes for table `congviec`
 --
 ALTER TABLE `congviec`
   ADD PRIMARY KEY (`MaCongViec`);
-
---
--- Indexes for table `luonghopdong`
---
-ALTER TABLE `luonghopdong`
-  ADD PRIMARY KEY (`MaHopDong`,`MaNV`);
 
 --
 -- Indexes for table `nguoithan`
@@ -386,8 +318,7 @@ ALTER TABLE `nguoithan`
 --
 ALTER TABLE `nhanvien`
   ADD PRIMARY KEY (`MaNV`),
-  ADD KEY `fkPhongbanNV` (`MaPhongBan`),
-  ADD KEY `fkChucvuNV` (`MaChucVu`);
+  ADD KEY `fkPhongbanNV` (`MaPhongBan`);
 
 --
 -- Indexes for table `phongban`
@@ -412,7 +343,7 @@ ALTER TABLE `tangca`
 -- Indexes for table `theodoitangluong`
 --
 ALTER TABLE `theodoitangluong`
-  ADD PRIMARY KEY (`MaHopDong`);
+  ADD PRIMARY KEY (`MaNV`);
 
 --
 -- Indexes for table `tienthuong`
@@ -427,6 +358,12 @@ ALTER TABLE `tongketchamcongthang`
   ADD PRIMARY KEY (`MaNV`);
 
 --
+-- Indexes for table `tongkettangca`
+--
+ALTER TABLE `tongkettangca`
+  ADD PRIMARY KEY (`MaNV`);
+
+--
 -- Constraints for dumped tables
 --
 
@@ -434,28 +371,20 @@ ALTER TABLE `tongketchamcongthang`
 -- Constraints for table `bangchamcong`
 --
 ALTER TABLE `bangchamcong`
-  ADD CONSTRAINT `fkChamcongNV` FOREIGN KEY (`MaNV`) REFERENCES `nhanvien` (`MaNV`);
+  ADD CONSTRAINT `fkbangchamcong` FOREIGN KEY (`manv`) REFERENCES `nhanvien` (`MaNV`);
 
 --
 -- Constraints for table `bangluongthang`
 --
 ALTER TABLE `bangluongthang`
-  ADD CONSTRAINT `fkHopdong` FOREIGN KEY (`MaHopDong`) REFERENCES `luonghopdong` (`MaHopDong`),
   ADD CONSTRAINT `fkPhucap` FOREIGN KEY (`MaPhuCap`) REFERENCES `phucap` (`MaPhuCap`),
   ADD CONSTRAINT `fkTangcaNV` FOREIGN KEY (`MaNV`) REFERENCES `nhanvien` (`MaNV`),
   ADD CONSTRAINT `fkTienthuong` FOREIGN KEY (`MaTienThuong`) REFERENCES `tienthuong` (`MaTienThuong`);
 
 --
--- Constraints for table `nguoithan`
---
-ALTER TABLE `nguoithan`
-  ADD CONSTRAINT `fkNguoiThanNV` FOREIGN KEY (`MaNV`) REFERENCES `nhanvien` (`MaNV`);
-
---
 -- Constraints for table `nhanvien`
 --
 ALTER TABLE `nhanvien`
-  ADD CONSTRAINT `fkChucvuNV` FOREIGN KEY (`MaChucVu`) REFERENCES `chucvu` (`MaChucVu`),
   ADD CONSTRAINT `fkPhongbanNV` FOREIGN KEY (`MaPhongBan`) REFERENCES `phongban` (`MaPhongBan`);
 
 --
@@ -463,12 +392,6 @@ ALTER TABLE `nhanvien`
 --
 ALTER TABLE `phongban`
   ADD CONSTRAINT `fkCongviecPB` FOREIGN KEY (`MaCongViec`) REFERENCES `congviec` (`MaCongViec`);
-
---
--- Constraints for table `theodoitangluong`
---
-ALTER TABLE `theodoitangluong`
-  ADD CONSTRAINT `fkTangluong` FOREIGN KEY (`MaHopDong`) REFERENCES `luonghopdong` (`MaHopDong`);
 
 --
 -- Constraints for table `tongketchamcongthang`
