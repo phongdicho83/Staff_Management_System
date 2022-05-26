029f5046ea227ca0c93de1eb12e38b0c410a0792
ngahehz <97083754+ngahehz@users.noreply.github.com>
2022-05-26 11:06:30 +0700

    Update CommonAttribute.java

--- src/staff_management_system/Helpers/CommonAttribute.java
+++ src/staff_management_system/Helpers/CommonAttribute.java
@@ -1,24 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package staff_management_system.Helpers;
 
 import java.util.ArrayList;
 import staff_management_system.DTO.BangChamCong;
 import staff_management_system.DTO.CongViec;
+import staff_management_system.DTO.LuongThang;
 import staff_management_system.DTO.NguoiThan;
 import staff_management_system.DTO.NhanVien;
 import staff_management_system.DTO.PhongBan;
+import staff_management_system.DTO.PhuCap;
+import staff_management_system.DTO.TangCa;
+import staff_management_system.DTO.TheoDoiTangLuong;
+import staff_management_system.DTO.ThuongLe;
+import staff_management_system.DTO.TongKetChamCongThang;
+import staff_management_system.DTO.TongKetTangCaThang;
 
-/**
- *
- * @author Utech
- */
 public class CommonAttribute {
-        public static ArrayList <NguoiThan> dsnt;
+        public static ArrayList <TangCa> dstc;
+        public static ArrayList <PhuCap> dspc;
+        public static ArrayList <ThuongLe> dstl;
+        public static ArrayList <CongViec> dscv;
         public static ArrayList <NhanVien> dsnv;
-        public static ArrayList <BangChamCong> dscc;
-        public static ArrayList <CongViec> dsvc;
         public static ArrayList <PhongBan> dspb;
+        public static ArrayList <NguoiThan> dsnt;
+        public static ArrayList <LuongThang> dslt;
+        public static ArrayList <BangChamCong> dscc;
+        public static ArrayList <TheoDoiTangLuong> dstdtl;
+        public static ArrayList <TongKetTangCaThang> dstktct;
+        public static ArrayList <TongKetChamCongThang> dstkcct;
 }
